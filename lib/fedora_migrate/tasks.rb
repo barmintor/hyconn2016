d7ab62a2a795793a644984753fa1c0335a73dcb9
Benjamin Armintor <armintor@gmail.com>
2016-09-30 07:32:40 -0400

    simple migration - common migration tasks

--- lib/fedora_migrate/tasks.rb
+++ lib/fedora_migrate/tasks.rb
@@ -1,6 +1,7 @@
 module FedoraMigrate
   module Tasks
     def self.pid_to_slug(pid)
+      return nil if pid.nil?
       pid.split(':').last
     end
     def self.connection
@@ -19,31 +20,40 @@ module FedoraMigrate
       solr_connection.delete_by_query("id:\"#{resource}\"", params: { 'softCommit' => true }) unless solr_connection.nil?
     end
     def self.destroy_previously_migrated(pid, container=nil)
-      resource = (container.nil?) ? "#{pid_to_slug(pid)}" : "#{pid_to_slug(container)}/#{pid_to_slug(pid)}"
+      prefix = ActiveFedora.config.credentials[:base_path]
+      path_segs = [prefix, pid_to_slug(container), pid_to_slug(pid)].compact!
+      resource = path_segs.join('/')
+      resource.sub!(/^\//,'')
       destroy_resource(resource)
     end
-    def self.migrate_common(pid)
-      puts "would have migrated #{pid}"
+    def self.migrate_common(pid, options={})
+      source = FedoraMigrate.source.connection.find(pid)
+      target = nil
+      mover = FedoraMigrate::ObjectMover.new(source, target, options)
+      mover.migrate
+      target = ActiveFedora::Base.find(mover.target.id)
+      FedoraMigrate::RelsExtDatastreamMover.new(source, target, options).migrate
+      nil
     end
-    def self.migrate_administrative_set(pid, reload=false, container=nil)
-      destroy_previously_migrated(pid,container) if reload
+    def self.migrate_administrative_set(pid, options={})
+      destroy_previously_migrated(pid,options[:container]) if options[:reload]
       # do the migration
-      migrate_common(pid)
+      migrate_common(pid, options)
     end
-    def self.migrate_file_set(pid, reload=false, container=nil)
-      destroy_previously_migrated(pid,container) if reload
+    def self.migrate_file_set(pid, options={})
+      destroy_previously_migrated(pid,options[:container]) if options[:reload]
       # do the migration
-      migrate_common(pid)
+      migrate_common(pid, options)
     end
-    def self.migrate_work(pid, reload=false, container=nil)
-      destroy_previously_migrated(pid,container) if reload
+    def self.migrate_work(pid, options={})
+      destroy_previously_migrated(pid,options[:container]) if options[:reload]
       # do the migration
-      migrate_common(pid)
+      migrate_common(pid, options)
     end
-    def self.migrate_collection(pid, reload=false, container=nil)
-      destroy_previously_migrated(pid,container) if reload
+    def self.migrate_collection(pid, options={})
+      destroy_previously_migrated(pid,options[:container]) if options[:reload]
       # do the migration
-      migrate_common(pid)
+      migrate_common(pid, options)
     end
   end
 end
