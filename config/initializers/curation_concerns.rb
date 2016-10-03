73ab8c6884e8da3b91519c8474febe3ba2629e05
Benjamin Armintor <armintor@gmail.com>
2016-10-02 23:47:51 -0400

    use the default AF uri translators

--- config/initializers/curation_concerns.rb
+++ config/initializers/curation_concerns.rb
@@ -53,6 +53,8 @@ CurationConcerns.configure do |config|
   # Leaving it blank will set the start date to when ever the file was uploaded by
   # NOTE: if you have always sent analytics to GA for downloads and page views leave this commented out
   # config.analytic_start_date = DateTime.new(2014,9,10)
+  config.translate_uri_to_id = ActiveFedora::Base.translate_uri_to_id
+  config.translate_id_to_uri = ActiveFedora::Base.translate_id_to_uri
 end
 
 Date::DATE_FORMATS[:standard] = '%m/%d/%Y'
