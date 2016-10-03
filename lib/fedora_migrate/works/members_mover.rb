18f206e200ec958b57fa96c94c4bb65386ab99cd
Benjamin Armintor <armintor@gmail.com>
2016-10-03 02:43:17 -0400

    migrate structure - modify MembersMover to parse structMetadata

--- lib/fedora_migrate/works/members_mover.rb
+++ lib/fedora_migrate/works/members_mover.rb
@@ -10,9 +10,28 @@ module FedoraMigrate
       def migrate_object(fc3_uri)
         ActiveFedora::Base.find(id_component(fc3_uri))
       end
+      def structured_members
+        structMetadata = source.datastreams['structMetadata']
+        return [] if structMetadata.nil? || structMetadata.new?
+        ns = {mets: "http://www.loc.gov/METS/"}
+        structMetadata = Nokogiri::XML(structMetadata.content)
+        members = {}
+        structMetadata.xpath("/mets:structMap/mets:div", ns).each do |node|
+          members[node["ORDER"]] = node["CONTENTIDS"]
+        end
+        sorted_keys = members.keys.sort {|a,b| a.to_i <=> b.to_i}
+        sorted_keys.map { |key| members[key] }
+      end
       def migrate_statements
-        statements.each do |statement|
-          target.members << migrate_object(statement.object)
+        unordered_members = statements.collect { |statement| statement.object.to_s }
+        ordered_members = structured_members & unordered_members
+        unordered_members = unordered_members - structured_members
+        ordered_members.each do |member|
+          next unless ActiveFedora::Base.exists?(id_component(member))
+          target.ordered_members << migrate_object(member)
+        end
+        unordered_members.each do |member|
+          target.members << migrate_object(member)
         end
         target.save # so that we don't have an Etag mismatch on update_index
       end
